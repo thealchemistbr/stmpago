84bd2311d6c4f7cb2c1d2feab989d2129d082348
Marcelo N. Magri <morbidsoftware@gmail.com>
2014-11-19 13:22:50 -0200

    initial release

---
+++ ChecarStatusMercadoPago/ChecarStatusMercadoPago/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using mercadopago;
+using System.Net;
+using System.IO;
+
+namespace stmpago
+{
+    class Program
+    {
+        static readonly string _CLIENT_ID = "3387529031021544";
+        static readonly string _CLIENT_SECRET = "ON3ELvqnODpbUdw5hLktjCUntq5NsX8I";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Status de pagamento - MercadoPago");
+            Console.WriteLine("");
+
+            MP mp = new MP(_CLIENT_ID, _CLIENT_SECRET);
+            mp.sandboxMode(true);
+            string _access_token = mp.getAccessToken();
+
+            Console.WriteLine("Access Token: {0}", _access_token);
+
+            string _url = "https://api.mercadolibre.com/sandbox/collections/1416402464?access_token=" + _access_token;
+
+            WebRequest req = WebRequest.Create(_url);
+
+            WebResponse resp = req.GetResponse();
+            Stream _stream = resp.GetResponseStream();
+            string _json = string.Empty;
+            using (StreamReader sr = new StreamReader(_stream))
+            {
+                _json = sr.ReadToEnd();
+            }
+
+            Console.WriteLine(_json);
+            Console.ReadLine();
+        }
+    }
+}
+
